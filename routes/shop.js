433a7396d9b7302d3c93de5780452c5c724ad1e6
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-02-22 17:42:02 +0530

    pug templete engine

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,15 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('../routes/admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    // console.log(adminData.products);
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {prods: products, pageTitle: 'My Shop', path: '/'});
 });
 
 module.exports = router;
