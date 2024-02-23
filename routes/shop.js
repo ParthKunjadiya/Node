c86a73f99d71dbce0655b3456a141445f83fcfbb
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-02-23 11:01:05 +0530

    handlebar templete engine

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,14 @@ router.get('/', (req, res, next) => {
     // console.log(adminData.products);
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'My Shop', path: '/'});
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'My Shop',
+        path: '/',
+        hasProduct: products.length > 0,
+        activeShop: true,
+        productCSS:true
+    });
 });
 
 module.exports = router;
