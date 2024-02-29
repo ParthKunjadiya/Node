225b0c7b6b9569b0e59aee24d07d73acb6012078
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-02-29 16:49:28 +0530

    adding more routes

--- models/product.js
+++ models/product.js
@@ -18,20 +18,33 @@ const getProductsFromFile = cb => {
 }
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, price, description) {
         this.title = title;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.description = description;
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
-                console.log(err);
+                if (err) {
+                    console.log(err);
+                }
             });
         });
     }
 
     static fetchAll(cb) {
         getProductsFromFile(cb)
+    }
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
     }
 }
