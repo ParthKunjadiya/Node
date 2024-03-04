6e1ae922c3bee4b390a1ff96ebc366d7f2915157
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-03-04 18:20:49 +0530

    use Sequelize

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -10,6 +15,16 @@ app.set('view engine', 'ejs');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { promises } = require('dns');
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,4 +34,35 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' }); // It create userId field in products table
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User); // It create userId field in carts table
+Cart.belongsToMany(Product, { through: CartItem }); // It create productId field in cartItems table
+Product.belongsToMany(Cart, { through: CartItem }); // It create cartId field in cartItems table
+
+// It create table for your define Models using sequelize.define() 
+sequelize
+    // force: true => create table whenever it run and delete table if it already exists else it overwrites it
+    // .sync({ force: true })
+    .sync()
+    .then(result => {
+        return User.findByPk(1);
+        console.log(result);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'Parth', email: 'parthkunjadiya3@gmail.com' });
+        }
+        return user;
+    })
+    .then(user => {
+        // console.log(user);
+        return user.createCart();
+    })
+    .then(cart => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
