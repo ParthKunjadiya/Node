d8e60ee72c7b06645c6febce1053670f86095195
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-03-05 18:14:37 +0530

    use MongoDb

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -22,52 +16,20 @@ const { promises } = require('dns');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//     User.findByPk(1)
+//         .then(user => {
+//             req.user = user;
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' }); // It create userId field in products table
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User); // It create userId field in carts table
-Cart.belongsToMany(Product, { through: CartItem }); // It create productId field in cartItems table
-Product.belongsToMany(Cart, { through: CartItem }); // It create cartId field in cartItems table
-Order.belongsTo(User); // It create userId field in orders table
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem }); // It create productId field in orderItems table
-
-// It create table for your define Models using sequelize.define() 
-sequelize
-    // force: true => create table whenever it run and delete table if it already exists else it overwrites it
-    // .sync({ force: true })
-    .sync()
-    .then(result => {
-        return User.findByPk(1);
-        // console.log(result);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({ name: 'Parth', email: 'parthkunjadiya3@gmail.com' });
-        }
-        return user;
-    })
-    .then(user => {
-        // console.log(user);
-        return user.createCart();
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+mongoConnect(() => {
+    app.listen(3000);
+});
--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node', 'root', 'Parth@123', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+    // '@' replace with '%40' in following URL
+    MongoClient.connect('mongodb+srv://Parth:%40Parth45%40@cluster0.tajycs5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        .then(client => {
+            console.log('Connected!');
+            _db = client.db();
+            callback();
+        })
+        .catch(err => {
+            console.log(err)
+            throw err;
+        });
+}
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No Database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
