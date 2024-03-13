b7b7b221ba6c08e2fc0034182fc83acfa42dfbbf
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-03-13 15:38:53 +0530

    Async Requests (deleting product from DOM)

--- app.js
+++ app.js
@@ -38,77 +38,83 @@ const fileFilter = (req, file, cb) => {
     }
 }
 
-    app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
-    const adminRoutes = require('./routes/admin');
-    const shopRoutes = require('./routes/shop');
-    const authRoutes = require('./routes/auth');
-    const { promises } = require('dns');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+const { promises } = require('dns');
 
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use('/images', express.static(path.join(__dirname, 'images')));
-    app.use(
-        session({
-            secret: 'my secret',
-            resave: false,
-            saveUninitialized: false,
-            store: store
-        })
-    );
-    app.use(csrfProtection);
-    app.use(flash());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer({
+    storage: fileStorage,
+    // limits: { fileSize: 1 * 1024 * 1024 }, // file size limit: 1MB
+    fileFilter: fileFilter
+}).single('image'));
+// .array('image', 10)); // this is multiple file upload configurations and in ejs file make input field attribute 'multiple'
 
-    app.use((req, res, next) => {
-        res.locals.isAuthenticated = req.session.isLoggedIn;
-        res.locals.csrfToken = req.csrfToken();
-        next();
-    });
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
+app.use(csrfProtection);
+app.use(flash());
 
-    app.use((req, res, next) => {
-        // sync error can reach to error handling middleware using throw new Error
-        // throw new Error('Sync Dummy');
-        if (!req.session.user) {
-            return next();
-        }
-        User.findById(req.session.user._id)
-            .then(user => {
-                // this with throw error to catch block
-                // throw new Error('Async Dummy');
-                if (!user) {
-                    return next();
-                }
-                req.user = user;
-                next();
-            })
-            .catch(err => {
-                // async error can reach to error handling middleware using next()
-                next(new Error(err));
-            });
-    });
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
-    app.use('/admin', adminRoutes);
-    app.use(shopRoutes);
-    app.use(authRoutes);
-
-    app.get('/500', errorController.get500);
-
-    app.use(errorController.get404);
-
-    app.use((error, req, res, next) => {
-        // res.status(error.httpStatusCode).render(...);
-        // res.redirect('/500');
-        res.status(500).render('500', {
-            pageTitle: 'Error!',
-            path: '/500'
-        });
-    });
-
-    mongoose.connect(MONGODB_URI)
-        .then(result => {
-            app.listen(3000);
+app.use((req, res, next) => {
+    // sync error can reach to error handling middleware using throw new Error
+    // throw new Error('Sync Dummy');
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+        .then(user => {
+            // this with throw error to catch block
+            // throw new Error('Async Dummy');
+            if (!user) {
+                return next();
+            }
+            req.user = user;
+            next();
         })
         .catch(err => {
-            console.log(err)
+            // async error can reach to error handling middleware using next()
+            next(new Error(err));
         });
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get('/500', errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+    // res.status(error.httpStatusCode).render(...);
+    // res.redirect('/500');
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500'
+    });
+});
+
+mongoose.connect(MONGODB_URI)
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err)
+    });
--- controllers/admin.js
+++ controllers/admin.js
@@ -170,8 +170,8 @@ exports.postEditProduct = (req, res, next) => {
         });
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId)
         .then(product => {
             if (!product) {
@@ -182,12 +182,10 @@ exports.postDeleteProduct = (req, res, next) => {
         })
         .then(result => {
             console.log('DESTROYED PRODUCT');
-            res.redirect('/admin/products');
+            res.status(200).json({ message: 'Deleting product success!' });
         })
         .catch(err => {
-            const error = new Error(err);
-            err.httpStatusCode = 500;
-            return next(error);
+            res.status(500).json({ message: 'Deleting product failed!' });
         });
 }
 
